0c678e1a22cc5cad7e1775d180e59f712ed3f56f
=Jonathan Loscalzo <=>
2017-06-02 17:11:00 -0300

    add container and css, separte of app in header, container and footer.

--- app/components/app.js
+++ app/components/app.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
-import Badge from './Badge/badge'
+import Popular from './popular';
+import Badge from './Badge/badge';
+
+class AppHeader extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello React Training!</h1>
+                <hr />
+            </div>
+        )
+    }
+}
+
+class AppFooter extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello React Training!</h1>
-        <hr />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <AppHeader />
+                <div className='container'>
+                    <Popular />
+                </div>
+                <AppFooter />
+            </div>
+        )
+    }
 }
