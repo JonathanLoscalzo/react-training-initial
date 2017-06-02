fb7dbca807a999b675fe0a4f883219dbde4fe7c3
=Jonathan Loscalzo <=>
2017-06-02 16:59:36 -0300

    separate of concerns - appjs in components

---
+++ app/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Badge from './Badge/badge'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello React Training!</h1>
+        <hr />
+      </div>
+    )
+  }
+}
--- app/index.js
+++ app/index.js
@@ -1,23 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import Badge from './components/Badge/badge'
 import './index.css';
-
-const myAvatar = "https://avatars0.githubusercontent.com/u/6944598?v=3&u=3ad8dbf6e5e8495a3790bd60150c168df6f9a40c&s=400";
-
-export class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello React Training!</h1>
-        <hr />
-        <Badge
-          img={myAvatar}
-          name="Nombre"
-          username="username" />
-      </div>
-    )
-  }
-}
+import App from './components/app'
 
 ReactDOM.render(<App />, document.getElementById('app'));
