018aa3f5c086b51c241bfc3cccf1dcbdcf2926de
Jonathan Loscalzo <loscalzo.jony@gmail.com>
2017-06-02 22:36:18 -0300

    add list of  possible languages

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,8 +1,70 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default class Popular extends React.Component {
+
+
+class SelectLanguage extends React.Component {
+
+    static propTypes = {
+        selected: PropTypes.string.isRequired,
+        onSelect: PropTypes.func.isRequired,
+    }
+
+    shouldComponentUpdate(prevState, nextState) {
+        return this.props.selected != prevState.selected
+    }
+
+    constructor(props) {
+        super(props);
+    }
 
     render() {
-        return (<div></div>);
+        var languages = [
+            "All",
+            "Javascript",
+            "Ruby",
+            "Java",
+            "CSS",
+            "Python"
+        ];
+
+        const {selected, onSelect} = this.props;
+
+        return (
+            <ul className="languages">
+                {languages.map(
+                    (lang) =>
+                        <li key={lang}
+                            onClick={() => onSelect(lang)}
+                            style={lang === selected ? { color: '#d0021b' } : null}>
+                            {lang}
+                        </li>)}
+            </ul>)
+    }
+
+}
+
+export default class Popular extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            selectLanguage: "All"
+        }
+    }
+
+    updateLanguage(lang) {
+        this.setState({
+            selectLanguage: lang,
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <SelectLanguage
+                    selected={this.state.selectLanguage}
+                    onSelect={(lang) => this.updateLanguage(lang)} />
+            </div>
+        );
     }
 }
